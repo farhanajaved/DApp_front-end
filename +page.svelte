afc207356cf0299944fa9079e397ed1c9a1042f5
Farhana Javed <87488893+farhanajaved@users.noreply.github.com>
2024-03-07 14:34:09 +0100

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import WalletConnect from '$lib/WalletConnect.svelte';
+	import LocalSign from '$lib/LocalSign.svelte';
+
+	export let web3Props: Web3Props;
+</script>
+
+{#if !web3Props?.account}
+	<div class="grid h-screen place-items-center">
+		<WalletConnect bind:web3Props />
+	</div>
+{:else}
+	<LocalSign bind:web3Props />
+{/if}
